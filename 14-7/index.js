c8fc9662992b7165528de9752bd0d75045c1d085
PriyanshuSaini02 <priyanshu688.be22@chitkara.edu.in>
2025-07-14 15:27:14 +0530

    server

---
+++ 14-7/index.js
@@ -0,0 +1,27 @@
+console.log("script js");
+
+const {sumOfTwo,xy}=require('./sumOfTwo.js')
+
+var xy=30000
+
+
+////calculator
+
+
+// function sumOfTwo(a,b){
+//     console.log(a,b)
+//     return a+b;
+// }
+
+// function mulOfTwo(a,b){
+//     console.log(a,b)
+//     return a*b;
+// }
+// function divOfTwo(a,b){
+//     console.log(a,b)
+//     return a/b;
+// }
+console.log(xy)
+console.log(sumOfTwo(10,20))
+// console.log(mulOfTwo(10,20))
+// console.log(divOfTwo(40,20))
